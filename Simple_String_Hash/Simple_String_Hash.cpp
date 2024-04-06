5d0db72378f7fd7280a47ce9555d2d400e9593fb
Mikhail <miktyu88@gmail.com>
2024-04-06 22:25:52 +0300

    Initial commit

---
+++ Simple_String_Hash/Simple_String_Hash.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+
+int simple_string_hash(std::string str) {
+
+    int hash = 0;
+
+    for (int i = 0; i < str.length(); i++) {
+        hash += static_cast<int>(str[i]);
+    }
+
+    return hash;
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Russian");
+    std::string str;
+
+    do {
+        std::cout << "Введите строку: ";
+        std::cin >> str;
+        std::cout << "Наивный хэш строки " << str << ": ";
+        std::cout << simple_string_hash(str) << std::endl;
+        std::cout << std::endl;
+
+    } while (str != "exit");
+
+    return 0;
+}
